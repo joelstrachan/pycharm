ecbddf6aa505bbb3085886e8981898334f19fe6e
joelstrachan <Eriamjh30>
2020-01-05 21:51:48 +1100

    router-os-update-script

--- Mikrotik/Update Router OS/router-os-update.py
+++ Mikrotik/Update Router OS/router-os-update.py
@@ -1,24 +1,51 @@
-def file_to_write():
+import random
+import string
+import paramiko
+import time
 
-    filename = input("please enter the name of the file you would like to write to")
+def settings():
+    router_ip_address = input(please enter the router ip address: )
+
+
+def file_create():
+
+    filename = input("please enter the name of the file you would like to write to: ")
     filename = filename + '.txt'
 
     print(f"Full file name is: {filename}")
 
-    file1 = open(file, 'a')
-
-    file1.write(f'/ip pool add name="{dhcp_pool_name}" ranges={dhcp_pool_ip_address_range} comment="{dhcp_pool_comment}" \n')
+    file1 = open(filename, 'a')
 
     file1.write(f'/ systemscheduler')
-
-
-    file1.write(f'add interval = 30d00: 00:00name = "Package upgrade" on - event = \')
-    file1.write(f'"system package update install" policy = \')
-    file1.write(f'ftp, reboot, read, write, policy, test, password, sniff, sensitive, romon \')
+    file1.write('\n')
+    file1.write(f'add interval=30d00:00:00 name="Package upgrade" on-event=\\')
+    file1.write('\n\t')
+    file1.write(f'"system package update install" policy = \\')
+    file1.write('\n\t')
+    file1.write(f'ftp, reboot, read, write, policy, test, password, sniff, sensitive, romon \\')
+    file1.write('\n\t')
     file1.write(f'start - time = 12:00: 00')
 
     file1.close()
 
-    
 
 
+
+def ssh(router_ip_address):
+    try:
+        session = paramiko.SSHClient()
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+        session.connect(router_ip_address, username=router_username, password=router_password)
+        selected_cmd_file = open(file, 'r')
+        selected_cmd_file.seek(0)
+        for each_line in selected_cmd_file.readlines():
+            connection = session.exec_command(each_line + '\n')
+            time.sleep(1)
+
+        selected_cmd_file.close()
+        session.close()
+        print("======================Configuration Done======================")
+    except paramiko.AuthenticationException:
+        print("* invalid username or password \n Please check the configuration.")
+
+file_create()
