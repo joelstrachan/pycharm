e1574a432fb92b44a3217ae6ed7d3e95f628ae60
joelstrachan <Eriamjh30>
2020-01-06 19:59:33 +1100

    Pycharm Projects

---
+++ Mikrotik/Update Router OS/testfile.py
@@ -0,0 +1,6 @@
+router_ip_address = input("please enter the ip address of the router: ")
+router_username = input("please enter username to access router: ")
+router_password = input("please enter password to access router: ")
+
+
+print(f"{router_ip_address}{router_username}{router_password}")
