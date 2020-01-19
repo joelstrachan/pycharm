e27b0f8415bea7fcdefd3263d3061a064951f9b5
joelstrachan <Eriamjh30>
2020-01-19 23:53:16 +1100

    Learn Python

---
+++ Learn Python3 the Hard Way/ex40a.py
@@ -0,0 +1,25 @@
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+
+
+
+
+#import ex40amystuff
+
+#ex40amystuff.apple()
+#print(ex40amystuff.tangerine)
+
+
+
+#mystuff = {'apple': 'I AM APPLES!'}
+#print(mystuff['apple'])
+
+#mystuff['apple'] # get apple from dict
+#mystuff.apple() # get apple from the module
+#mystuff.tangerine # same thing, it's just a variable
